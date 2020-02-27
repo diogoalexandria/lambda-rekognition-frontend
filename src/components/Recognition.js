b8066285c1161a7dd618c610f460e6e438b181ad
Alan Ferreira <aferreirasv@gmail.com>
2020-02-27 16:24:42 -0300

    Procura rosto na collection

--- src/components/Recognition.js
+++ src/components/Recognition.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Webcam from 'react-webcam'
 import { Rekognition } from 'aws-sdk';
-import { IconButton, Grid, createMuiTheme } from '@material-ui/core'
+import { IconButton, Grid, createMuiTheme, Typography } from '@material-ui/core'
 import { PhotoCamera } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/styles'
 
-export default function Recognition(){
-    const awsCredentials={       
+export default function Recognition() {
+
+    const [nickname, setNickname] = React.useState('')
+    const awsCredentials = {
         accessKeyId: process.env.REACT_APP_AWS_ACESS_KEY_ID,
         secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACESS_KEY,
         region: 'us-east-1'
     }
     const rekognition = new Rekognition(awsCredentials)
-    
+
     const webcamRef = React.useRef(null)
     const theme = createMuiTheme()
     const useStyles = makeStyles({
@@ -28,11 +30,11 @@ export default function Recognition(){
         }
 
     })
-    
+
     const rekognizeImage = params => {
-        rekognition.searchFacesByImage(params, (err,data)=>{
-            if(err) console.log(err,err.stack)
-            else console.log(data)
+        rekognition.searchFacesByImage(params, (err, data) => {
+            if (err) console.log(err, err.stack)
+            else setNickname(data.FaceMatches[0].Face.ExternalImageId)
         })
     }
 
@@ -47,10 +49,23 @@ export default function Recognition(){
         return new Blob([arrayBuffer], { type: 'image/jpeg' })
     }
 
+
+    const getBinary = (b64img) => {
+        let binaryImg = atob(b64img.split(',')[1]);
+        let length = binaryImg.length
+        let ab = new ArrayBuffer(length);
+        let ua = new Uint8Array(ab)
+        for (let i = 0; i < length; i++) {
+            ua[i] = binaryImg.charCodeAt(i);
+        }
+        return ab
+
+    }
+
     const handleSubmit = e => {
         let imgSrc = webcamRef.current.getScreenshot()
-        let img = b64ToBlob(imgSrc)
-        console.log(Buffer.from(imgSrc))
+        let img = getBinary(imgSrc)
+        console.log(img)
         var params = {
             CollectionId: "lambda-talks",
             Image: {
@@ -64,7 +79,7 @@ export default function Recognition(){
     const classes = useStyles()
     return (
         <React.Fragment>
-            <Grid container 
+            <Grid container
                 direction="column"
                 justify="center"
                 alignItems="center"
@@ -82,8 +97,11 @@ export default function Recognition(){
                 </Grid>
                 <Grid item>
                     <IconButton onClick={handleSubmit} className={classes.button}>
-                        <PhotoCamera/>
+                        <PhotoCamera />
                     </IconButton>
+                </Grid>
+                <Grid item>
+                    <Typography hidden={!nickname}>Olá, {nickname}!</Typography>
                 </Grid>
             </Grid>
         </React.Fragment>
